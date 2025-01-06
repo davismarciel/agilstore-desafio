3e15b806e9a572c4409cb162d382963998d60e50
davismarciel <davisamarciel@gmail.com>
2025-01-06 16:44:06 -0300

    chore: initial spring boot project configurations

---
+++ src/main/java/com/davi/agileStore/AgileStoreApplication.java
@@ -0,0 +1,13 @@
+package com.davi.agileStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/agileStore/AgileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.agileStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
